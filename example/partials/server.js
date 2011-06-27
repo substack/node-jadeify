0338b2ee4914ea73222b4845a4e0387e189714d4
James Halliday <mail@substack.net>
2011-06-27 14:14:30 -0700

    partial example works again

--- example/partials/server.js
+++ example/partials/server.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var app = express.createServer();
+var util = require('util');
 
 app.use(express.static(__dirname + '/static'));
-app.listen(8080);
-console.log('Listening on 8080');
 
 app.get('/', function (req, res) {
     res.render('index.jade', { layout : false });
 });
 
 var browserify = require('browserify');
-var bundle = browserify({
-    entry : __dirname + '/static/main.js',
-    require : [
-        'deck',
-        { jquery : 'jquery-browserify' }
-    ]
-});
-app.use(bundle);
-
 var jadeify = require('jadeify');
-bundle.use(jadeify(__dirname + '/views'));
+
+util.print('Generating bundle... ');
+var bundle = browserify()
+    .use(jadeify(__dirname + '/views'))
+    .addEntry(__dirname + '/static/main.js')
+;
+console.log('done');
+
+app.use(bundle);
+app.listen(8080);
+console.log('Listening on 8080');
